f183d09ff33c1a04cd72e3092eeac93e7b5a3776
Purva25Thakare <thakarepurva25@gmail.com>
2022-06-26 18:05:06 +0530

    Added:Welcome Message

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,25 @@
+package bridzelabz.day1emp;
+
+public class EmployeeWageBuilder {
+public static void main(String[] args)
+{
+	System.out.println("Hello Welcome to Employee wage Calculator");
+	int ISPRESENT = 1;
+	int Wagephour = 20;
+	int hours = 8;
+	double empcheck=Math.floor(Math.random() * 10 % 2);
+	int empwage = Wagephour * hours ;
+	if(empcheck == ISPRESENT) {
+	System.out.println("Employee is Present");
+	System.out.println ("Wage of Employee" + empwage);
+	}
+		else {
+			
+		
+			System.out.println("Employee is Absent");
+	
+		}
+	
+}
+
+}
