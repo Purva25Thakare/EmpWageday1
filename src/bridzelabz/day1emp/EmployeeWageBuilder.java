d373785ef35067c00fc28774ed0a514b2d2593ac
Purva25Thakare <thakarepurva25@gmail.com>
2022-06-26 18:16:07 +0530

    Added:Employee Full time Wage Calculator

--- src/bridzelabz/day1emp/EmployeeWageBuilder.java
+++ src/bridzelabz/day1emp/EmployeeWageBuilder.java
@@ -6,13 +6,13 @@ public static void main(String[] args)
 	System.out.println("Hello Welcome to Employee wage Calculator");
 	
 	int ISPRESENT = 1;
-	/*int Wagephour = 20;
-	int hours = 8;*/
+	int Wagephour = 20;
+	int hours = 8;
 	double empcheck=Math.floor(Math.random() * 10 % 2);
-	/*int empwage = Wagephour * hours ;*/
+	int empwage = Wagephour * hours ;
 	if(empcheck == ISPRESENT) {
 	System.out.println("Employee is Present");
-	/*System.out.println ("Wage of Employee" + empwage);*/
+	System.out.println ("Wage of Employee" + empwage);
 	}
 		else {
 			
