4dfda0aa498795a31fb0c1e0808d82a5374f59f9
Purva25Thakare <thakarepurva25@gmail.com>
2022-06-26 18:58:03 +0530

    Added:Monthly Wage

--- src/bridzelabz/day1emp/EmployeeWageBuilder.java
+++ src/bridzelabz/day1emp/EmployeeWageBuilder.java
@@ -7,15 +7,18 @@ public static void main(String[] args)
 	
 	int ISPRESENT = 1;
 	int Wagephour = 20;
-	/*int hours = 8;*/
-	int parthrs=4;
+	int hours = 8;
+	int workingdays = 20;
+	/*int parthrs=4;*/
 	double empcheck=Math.floor(Math.random() * 10 % 2);
 	/*int empwage = Wagephour * hours ;*/
-	int wagepartime = parthrs * Wagephour ;
+	/*int wagepartime = parthrs * Wagephour ;*/
+	int monthwage= workingdays * hours * Wagephour ;
 	if(empcheck == ISPRESENT) {
 	System.out.println("Employee is Present");
-	/*System.out.println ("Wage of Employee" + empwage);*/
-	System.out.println ("Wage of Part time Employee" + wagepartime);
+	/*System.out.println ("Wage of Employee" + empwage);
+	System.out.println ("Wage of Part time Employee" + wagepartime);*/
+	System.out.println (" Monthly Wage of Employee" + monthwage);
 	}
 		else {
 			
